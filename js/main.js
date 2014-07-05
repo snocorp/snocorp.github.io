b9e0e32f393025814650e4eb042f4f06dda381da
Dave Sewell <snocorp@gmail.com>
2014-07-04 23:07:34 -0400

    Website refresh

--- js/main.js
+++ js/main.js
@@ -1,13 +1,15 @@
-
-jQuery(document).ready(function($) {
-  var sections = ['life', 'code', 'work'];
-  for (var i in sections) {
-    $('#'+sections[i]+' h2, #'+sections[i]+'Link').click(function(section) { return function(event) {
-      $('section.active').removeClass('active');
-      $('#'+section).addClass('active');
-
-      console.log(section);
-      event.preventDefault();
-    };}(sections[i]));
-  }
-});
+function code() {
+    document.getElementById('code').className = 'selected';
+    document.getElementById('life').className = '';
+    document.getElementById('work').className = '';
+}
+function life() {
+    document.getElementById('code').className = '';
+    document.getElementById('life').className = 'selected';
+    document.getElementById('work').className = '';
+}
+function work() {
+    document.getElementById('code').className = '';
+    document.getElementById('life').className = '';
+    document.getElementById('work').className = 'selected';
+}
